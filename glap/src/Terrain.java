c532b81a60c126d70e4488e7b3eafb98ecb21cd5
ColaMachine <371452875@qq.com>
2014-08-26 23:48:38 +0800

    1
    1

---
+++ glap/src/Terrain.java
@@ -0,0 +1,99 @@
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+
+public class Terrain {
+	private String bmp_path="";
+	public static void main(String args[]){
+		//根据路径创建维图大小创建组
+		try {
+			File _file = new File("1.jpg"); //读入文件
+			BufferedImage bi=ImageIO.read(new File(""));
+			Image src = javax.imageio.ImageIO.read(_file); //构造Image对象
+
+			int width=src.getWidth(null); //得到源图宽
+
+			int height=src.getHeight(null); //得到源图长
+			
+			float[][] heights  =  new float[width][height];
+			  byte lowest =(byte) 255;
+		        byte hightest = 0;
+		        
+		        for(int i=0;i <width;i++  )    {
+		            for(int j=0;j   <height;j++)         {
+		                if (heights[i][j]  < lowest)
+		                    lowest =heights[i][j] ;
+		                if( heights[i][j]  > hightest)
+		                    hightest = heights[i][j];
+		                //由于每个像素是24位，而指针是8位，所以+3指向下一个像素
+		               
+		            }
+		        }
+		        
+		        for(int i=0;i< heightMap.Width;i++)
+		        {
+		            for(int j=0; j< heightMap.Height; j++)
+		            {
+		                heights[i,j] = (float)(*p - lowest) / (float)(hightest - lowest) * (Max - Min) + Min;
+		                p += 3;
+		            }
+		        }
+		        
+		        heightMap.UnlockBits(data);
+		        //计算顶点，索引，三角形数量
+		        numVertices = heightMap.Width * heightMap.Height;
+		        numIndices = 6 * (heightMap.Width - 1) * (heightMap.Height - 1);
+		        numTriangles = 2 * (heightMap.Width - 1) * (heightMap.Height - 1);
+		        //创建顶点数组
+		        Vector3[] verts = new Vector3[numVertices];
+		        int[] index = new int[numIndices];
+		        int x = 0;
+		        int n = 0;
+		        float dx = terrainSize / (float) heightMap.Height;
+		        float dy = terrainSize / (float) heightMap.Width;
+		        //填充顶点数组
+		        for ( int i = 0; i < heightMap.Height; i ++)
+		        {
+		            for ( int j = 0; j < heightMap.Width; j ++)
+		            {
+		                verts[i*heightMap.Width+j] = new Vector3((float)j*dx -terrainSize/2f,heights[j,i],(float)i*dy -terrainSize/2f);
+		            }
+		        }
+		        //填充索引数组
+		        for ( int i = 0; i < heightMap.Width-1; i ++)
+		        {
+		            for ( int j = 0; j < heightMap.Height-1; j ++)
+		            {
+		                x = i * heightMap.Width + j;
+		                index[n++] = x;
+		                index[n++] = x+1;
+		                index[n++] = x+heightMap.Width+1;
+		                index[n++] = x;
+		                index[n++] = x+heightMap.Width;
+		                index[n++] = x+heightMap.Width+1;
+		            }
+		        }
+		        //设置顶点以及索引缓冲
+		        vb = new VertexBuffer(typeof(Vector3),numVertices,device,Usage.None,VertexFormats.Position,Pool.Default);
+		        vb.SetData(verts,0,0);
+		        ib = new IndexBuffer(typeof(int),numIndices,device,Usage.None,Pool.Default);
+		        ib.SetData(index,0,0);
+		} catch (IOException e) {
+			// VIP Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	   public void DrawTerrain()
+	    {
+	        device.VertexFormat = VertexFormats.Position;
+	        device.SetStreamSource(0,vb,0);
+	        device.Indices = ib;
+	        device.Transform.World = Matrix.Translation(0,0,0);
+	        device.DrawIndexedPrimitives(PrimitiveType.TriangleList,0,0,numVertices,0,numTriangles);
+	    }
+}
